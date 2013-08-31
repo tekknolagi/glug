031f323abebe3a3fecbc2f2c9c091a3bbcf42e12
Max Bernstein <tekknolagi@gmail.com>
2013-08-31 16:35:51 -0700

    shitty image support

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 require 'cgi'
+require 'base64'
 
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_NAVY_URL'] || ENV['GULAGRB_POSTGRESQL_URL'])
 
@@ -17,6 +18,7 @@ class Post
   end
 
   has n, :comments
+  has 1, :image
 end
 
 class Comment
@@ -24,12 +26,27 @@ class Comment
 
   property :id, Serial
   property :uid, String, :length => 32, :default => lambda { |r, p| Digest::MD5.hexdigest(r.created_at.to_s+r.body) }
-  property :body, String
+  property :body, Text
   property :created_at, DateTime, :default => ->(r, p) { DateTime.now }
 
   def nicedate
     created_at.strftime("%I:%M%P on %A %B %d, %Y")
   end
+
+  has 1, :image
+end
+
+class Image
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :uid, String, :length => 32, :default => lambda { |r, p| Digest::MD5.hexdigest(r.created_at.to_s+r.file) }
+  property :file, Text
+  property :type, String
+  property :created_at, DateTime, :default => ->(r, p) { DateTime.now }
+
+  belongs_to :comment, :required => false
+  belongs_to :post, :required => false
 end
 
 DataMapper.auto_upgrade!
@@ -43,6 +60,8 @@ def bad(content)
 end
 
 class GulagApp < Sinatra::Application
+  include FileUtils::Verbose
+
   helpers do
     def current_page
       request.path_info
@@ -60,16 +79,36 @@ class GulagApp < Sinatra::Application
     erb :single
   end
 
+  get '/i/:uid' do
+    if (i = Image.first(:uid => params[:uid]))
+      response.headers['Content-Type'] = i.type
+      Base64.decode64(i.file)
+    else
+      Base64.decode(File.read("404.txt"))
+    end
+  end
+
   post '/new' do
     if bad(params[:post]) or bad(params[:comment])
       puts params[:post]
       puts params[:comment]
       redirect to(params[:orig] || "/")
     end
+    if params[:image] && params[:image][:tempfile]
+      @file = true
+      b64 = [File.read(params[:image][:tempfile])].pack("m")
+      puts b64
+      i = Image.create(:file => b64, :type => params[:image]['Content-Type'])
+      puts i
+    end
     p = Post.create(:title => params[:post])
     c = Comment.new(:body => params[:comment])
     p.comments << c
     c.save
+    if @file
+      p.image = i
+      i.save
+    end
     redirect to("/p/#{p.uid}")
   end
 
