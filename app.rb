79ded72276b8afc88be8847b8f8a9cbfe4860190
Max Bernstein <tekknolagi@gmail.com>
2013-09-02 22:46:31 -0700

    images and bootstrap... ephemeral filesystem

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require 'data_mapper'
+require 'carrierwave'
 require 'carrierwave/datamapper'
 require 'cgi'
 require 'base64'
+require 'pry'
 
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_NAVY_URL'] || ENV['GULAGRB_POSTGRESQL_URL'])
 
@@ -37,45 +39,43 @@ class Comment
   has 1, :image
 end
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider           => 'Rackspace',
-    :rackspace_username => ENV['RACKSPACE_USER'],
-    :rackspace_api_key  => ENV['RACKSPACE_API_KEY']
-  }
 
-  config.fog_directory = ENV['RACKSPACE_CONTAINER']
-  config.asset_host = ENV['RACKSPACE_ASSET_HOST']
+CarrierWave.configure do |config|
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
 
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
+  include CarrierWave::MiniMagick
 
   process :set_content_type
 
-  storage :fog
-
-  def store_dir 
-   # "uploads/#{image.id+image.filename}"
+  def store_dir
     "uploads"
+  end
+  
+  version :thumb do
+    process :resize_to_fill => [200,132]
   end
 end
 
 class Image
   include DataMapper::Resource
+  extend CarrierWave::Mount
+
   mount_uploader :source, ImageUploader
 
   property :id, Serial
   property :created_at, DateTime, :default => ->(r, p) { DateTime.now }
 
-  property :filename, String
-  property :name, String
-  property :tempfile, Text
-  property :type, String
-  property :head, Text
+#  property :filename, String
+#  property :name, String
+#  property :tempfile, Text
+#  property :type, String
+#  property :head, Text
 
-  property :url, Text, :length => 100
+  property :url, Text, :length => 200
+  property :thumb, Text, :length => 200
 
   belongs_to :comment, :required => false
   belongs_to :post, :required => false
@@ -112,7 +112,7 @@ class GulagApp < Sinatra::Application
   end
 
   post '/new' do
-    if bad(params[:post]) or bad(params[:comment])
+    if bad(params[:post])
       redirect to(params[:orig] || "/")
     end
     p = Post.new(:title => params[:post])
@@ -120,8 +120,8 @@ class GulagApp < Sinatra::Application
     p.comments << c
     if params[:image] && params[:image][:tempfile]
       i = ImageUploader.new
-      i.store! File.open(params[:image][:tempfile])
-      p.image = Image.create(:url => i)
+      i.store! params[:image]
+      p.image = Image.create(:url => i.url, :thumb => i.thumb.url)
       p.save!
     end
     p.save!
