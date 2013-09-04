16fd1a2d0e0f8ba3ae7bbd978728bdd3c250a823
Max Bernstein <tekknolagi@gmail.com>
2013-09-03 18:23:55 -0700

    fix js, add ul class

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 $(window).load(function() {
     if ($('#new')) $('#new').focus();
-    $(function() {
-	var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
-	$('#target').html($('#source').html().replace(urlRegEx, "<a href='$1'>$1</a>"));
-    });  
+    
+    var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
+    $(".comments li").each(function(elNum) {
+	$(this).html($(this).html().replace(urlRegEx, "<a href='$1' target='_blank'>$1</a>"));
+    });
 });
